4cc03e1fe7d6ee71a5e5b0baaf7a7394228dc0da
John Eckert <johnteckert@gmail.com>
2020-05-01 16:42:44 -0400

    Updates

---
+++ precache-manifest.a58f2860a2ddb3779962412a594ac632.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49b26854e09a0a62baa921376b6deaa1",
+    "url": "/turnip-fam/index.html"
+  },
+  {
+    "revision": "490b136cab9346f0f9c9",
+    "url": "/turnip-fam/static/css/main.d5eee1c3.chunk.css"
+  },
+  {
+    "revision": "14a9a65314e9e28fc5f3",
+    "url": "/turnip-fam/static/js/2.dee55861.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/turnip-fam/static/js/2.dee55861.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "490b136cab9346f0f9c9",
+    "url": "/turnip-fam/static/js/main.3e4cdd1a.chunk.js"
+  },
+  {
+    "revision": "f4a9dc7ad3fb8e5a27d8",
+    "url": "/turnip-fam/static/js/runtime-main.422d4646.js"
+  }
+]);
